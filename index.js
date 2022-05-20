ee312c164a771b71e95e5d87f65241386e0b52c1
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-20 16:01:43 +0530

    images-commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express=require("express")
 var app=express()
 var keypeople= require("./server/routes/keypeople")
+var path = require("path")
 
 app.use("/keypeople",keypeople)
 
 app.get("/",function(request,response){
     response.send("Node JS App is running")
+})
+
+app.get("/images/:imagename",function(request,response){
+    let imagename = request.params.imagename
+    response.sendFile(path.join(__dirname,"images/"+imagename))
 })
 
 app.listen("8000",function(){
